a729af3b8f13a09dc419061a6cdc8e684123e452
Dt02code <dtan28206@gmail.com>
2026-05-04 17:42:58 +0700

    Delete connect.php

--- web/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("127.0.0.1", "root", "", "cellphone_k", 3307);
-
-if (!$conn) {
-    echo "<b style='color:red'>Loi ket noi </b>" . mysqli_connect_error();
-    exit();
-}
