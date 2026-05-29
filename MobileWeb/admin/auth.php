2a602158c72d713f5169a2452c855f5548523cdc
phamdangkhoa36715 <phamdangkhoa36715@gmail.com>
2026-05-29 16:41:11 +0700

    Delete MobileWeb directory

--- MobileWeb/admin/auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Guard file: include vào đầu mỗi trang admin
-if (session_status() === PHP_SESSION_NONE) session_start();
-if (empty($_SESSION['admin'])) {
-    header('Location: login.php');
-    exit;
-}
--- MobileWeb/admin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['admin'], $_SESSION['admin_name']);
-session_destroy();
-header('Location: login.php');
-exit;
