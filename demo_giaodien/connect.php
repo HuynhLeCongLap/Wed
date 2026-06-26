7f3ab476407c3a7c4de95598b2db58c61c802cec
HuynhLeCongLap <164972544+HuynhLeCongLap@users.noreply.github.com>
2026-06-26 18:29:26 +0700

    Delete demo_giaodien directory

--- demo_giaodien/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "cellphone_k");
-
-if (!$conn) {
-    echo "<b style='color:red'>Loi ket noi </b>" . mysqli_connect_error();
-    exit();
-}
--- demo_giaodien/dang-xuat.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-require_once __DIR__ . '/config/app.php';
-
-unset($_SESSION['user_id']);
-flash('success', 'Đã đăng xuất.');
-redirect('index.php');
